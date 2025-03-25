da671c1f4ede8862b24c76fff3ba6e6989aae7e1
Anthony <145694891+smartik77@users.noreply.github.com>
2025-03-25 18:07:02 +0300

    Create js1.js

---
+++ js1.js
@@ -0,0 +1,30 @@
+function createPoint(x, y) {
+    return {
+        x, y,
+
+        getX() {
+            return this.x
+        },
+
+        getY() {
+            return this.y
+        },
+
+        translate(dx, dy) {
+            this.x += dx
+            this.y += dy
+        },
+
+        scale(num) {
+            this.x *= num
+            this.y *= num
+        }
+    }
+}
+
+let point = createPoint(4, 3)
+
+console.log(point.getX(), point.getY())
+
+point.translate(1, -1)
+console.log(point.getX(), point.getY())
