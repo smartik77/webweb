38cbae76e3f3596333aef89b4d5ea9b279a6ca77
Anthony <145694891+smartik77@users.noreply.github.com>
2025-03-26 18:40:26 +0300

    Update js6.js

--- js6.js
+++ js6.js
@@ -1,21 +1,28 @@
-const withGreeter = base => class extends base {
-     greet(greeting) {
+// Принимает базовый класс и возвращает новый класс с методом greet
+const withGreeter = (base) => class extends base {
+    greet(greeting) {
+        // Метод использует свойство name из родительского класса
         return `${greeting}, ${this.name}!`
-  }
+    }
 }
 
+// Базовый класс Employee
 class Employee {
     constructor(name, salary) {
         this.name = name
-        this.salary = salary
+        this.salary = salary 
     }
+    
     raiseSalary(percent) {
         this.salary *= 1 + percent / 100
     }
 }
-
-
  
+// Создаем расширенную версию Employee с методом greet
 const GreetableEmployee = withGreeter(Employee)
-const e = new GreetableEmployee('Harry Smith', 90000)
-console.log(e.greet('Hello'))
+
+// Создаем объект расширенного класса
+const harry = new GreetableEmployee('Harry Smith', 90000)
+
+// Вызываем новый метод greet
+console.log(harry.greet('Hello'))
