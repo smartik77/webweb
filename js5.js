68ef3420b84d80e0db3f4e14a63eb6849e5d20c2
Anthony <145694891+smartik77@users.noreply.github.com>
2025-03-26 18:31:55 +0300

    Update js5.js

--- js5.js
+++ js5.js
@@ -1,13 +1,19 @@
 function createGreetable(str) {
+    // Создаём обёртку над строкой
     const result = new String(str)
+    
+    // Сначала делаем greetablePrototype наследником String.prototype
     Object.setPrototypeOf(greetablePrototype, String.prototype)
+    
+    // Затем делаем result наследником greetablePrototype
     return Object.setPrototypeOf(result, greetablePrototype)
 }
 
+// Создаём объект-прототип с методом greet
 const greetablePrototype = {
-        greet(greeting) {
-            return `${greeting}, ${this}!`
-        }
+    greet(greeting) {
+        return `${greeting}, ${this}!`  // this ссылается на объект-строку
+    }
 }
 
 const g = createGreetable('World')
