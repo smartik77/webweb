404a02928c5ab4fbf9f5f89fd05035ad68858285
Anthony <145694891+smartik77@users.noreply.github.com>
2025-03-26 18:08:33 +0300

    Update js2.js

--- js2.js
+++ js2.js
@@ -1,21 +1,26 @@
+// Point - функция создания точки на плоскости с координатами x и y
 function Point(x, y) {
     this.x = x
     this.y = y
 }
 
+// Point.prototype.getX - метод получения х
 Point.prototype.getX = function() {
     return this.x
 }
 
+// Point.prototype.getY - метод получения y
 Point.prototype.getY = function() {
     return this.y
 }
 
+// translate прибавляет к координатам точки указанные числф dx и dy
 Point.prototype.translate = function(dx, dy) {
     this.x += dx
     this.y += dy
 }
 
+// scale умножает координаты точки на указанное число num
 Point.prototype.scale = function(num) {
     this.x *= num
     this.y *= num
