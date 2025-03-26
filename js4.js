8894944464772f1f5872e9948f1ed6c0002ae6c6
Anthony <145694891+smartik77@users.noreply.github.com>
2025-03-26 18:00:04 +0300

    Update js4.js

--- js4.js
+++ js4.js
@@ -4,21 +4,25 @@ class Point {
         this.y = y
     }
 
+    // get x - акцессор получения свойства x
     get x() {
         return this._x
     }
 
+    // set x - акцессор изменения свойства x
     set x(value) {
         if (typeof value !== 'number') {
             console.log("Не число")
         }
         this._x = value
     }
-
+    
+    // get y акцессор получения свойства y
     get y() {
         return this._y
     }
 
+    // set y акцессор изменения свойства y
     set y(value) {
         if (typeof value !== 'number') {
             console.log("Не число")
