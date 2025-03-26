51e489c75f51b01e7801953a53cbb7c918b73496
Anthony <145694891+smartik77@users.noreply.github.com>
2025-03-26 18:04:08 +0300

    Update js1.js

--- js1.js
+++ js1.js
@@ -1,21 +1,26 @@
 // createPoint создает точку на плоскости с координатами x, y 
 function createPoint(x, y) {
     return { 
-        x, y,
+        x,  // координата х
+        y,  // координата у
 
+        // getX - метод получения х
         getX() {
             return this.x
         },
 
+        // getУ - метод получения у
         getY() {
             return this.y
         },
 
+        // translate прибавляет к координатам точки указанные числф dx и dy
         translate(dx, dy) {
             this.x += dx
             this.y += dy
         },
 
+        // scale умножает координаты точки на указанное число num
         scale(num) {
             this.x *= num
             this.y *= num
