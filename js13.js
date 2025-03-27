720062d55c1c481c2bbf0c9c21381f20c713d993
Vladislav Safronov <119115626+Vladislav-Safronov@users.noreply.github.com>
2025-03-27 19:30:23 +0300

    Create js13.js

---
+++ js13.js
@@ -0,0 +1,31 @@
+class BankAccount {
+  constructor(balance = 0) {
+    this.balance = balance;
+  }
+
+  deposit(amount) {
+    if (amount > 0) {
+      this.balance += amount;
+      return true;
+    }
+    return false;
+  }
+}
+
+
+// В JavaScript методы объектов (включая методы класса) теряют контекст (this), если их вызывать отдельно от объекта. 
+// Когда мы присваиваем метод переменной action и вызываем его как обычную функцию, this внутри метода перестает ссылаться на harrysAccount.
+
+// const harrysAccount = new BankAccount(100);
+// const action = harrysAccount.deposit; 
+// action(1000); // Ошибка: this внутри deposit станет undefined (в strict mode)
+
+
+
+
+// можно исправить код так:
+const harrysAccount = new BankAccount(100);
+harrysAccount.deposit.call(harrysAccount, 200); 
+harrysAccount.deposit.apply(harrysAccount, [1000]);
+
+console.log(harrysAccount)
