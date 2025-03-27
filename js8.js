9cc478cea810913fe8b035b73e030b6e93b522f8
Anthony <145694891+smartik77@users.noreply.github.com>
2025-03-27 20:41:12 +0300

    Update js8.js

--- js8.js
+++ js8.js
@@ -1,27 +1,34 @@
+// Функция для создания родительского узла, возвращает дочерние узлы
 const createParent = (value, children) => ({
-  value,
-  children,
+  value,       // Значение узла
+  children,    // Массив дочерних узлов
+  // Метод вычисления глубины поддерева
   depth() {
+    // 1 (текущий узел) + максимальная глубина дочерних поддеревьев
     return 1 + Math.max(...this.children.map(n => n.depth()))
   }
 })
 
-const createLeaf = value => ({
+// Функуия создания листового узла
+const createLeaf = (value) => ({
   value,
+  // Глубина листа всегда = 1
   depth() { return 1 }
 })
 
-const tree = createParent('root', [
-  createParent('child1', [
-    createLeaf('leaf1'),
+// Создание древовидной структуры:
+const tree = createParent('root', [  // Корневой узел
+  createParent('child1', [           // Узел уровня 2
+    createLeaf('leaf1'),             // Листья уровня 3
     createLeaf('leaf2')
   ]),
-  createLeaf('child2'),
-  createParent('child3', [
-    createParent('child3.1', [
-      createLeaf('leaf3')
+  createLeaf('child2'),              // Лист уровня 2
+  createParent('child3', [           // Узел уровня 2
+    createParent('child3.1', [       // Узел уровня 3
+      createLeaf('leaf3')            // Лист уровня 4
     ])
   ])
 ])
 
+// Вычисление глубины всего дерева
 console.log(tree.depth())
