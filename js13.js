ba9016458ae1259bdd3c03b0794897fd58ccd517
Anthony <145694891+smartik77@users.noreply.github.com>
2025-03-28 12:34:21 +0300

    Update js13.js

--- js13.js
+++ js13.js
@@ -1,14 +1,14 @@
 class BankAccount {
   constructor(balance = 0) {
-    this.balance = balance;
+    this.balance = balance
   }
 
   deposit(amount) {
     if (amount > 0) {
-      this.balance += amount;
-      return true;
+      this.balance += amount
+      return true
     }
-    return false;
+    return false
   }
 }
 
@@ -16,16 +16,16 @@ class BankAccount {
 // В JavaScript методы объектов (включая методы класса) теряют контекст (this), если их вызывать отдельно от объекта. 
 // Когда мы присваиваем метод переменной action и вызываем его как обычную функцию, this внутри метода перестает ссылаться на harrysAccount.
 
-// const harrysAccount = new BankAccount(100);
-// const action = harrysAccount.deposit; 
-// action(1000); // Ошибка: this внутри deposit станет undefined (в strict mode)
+// const harrysAccount = new BankAccount(100)
+// const action = harrysAccount.deposit 
+// action(1000) // Ошибка: this внутри deposit станет undefined (в strict mode)
 
 
 
 
 // можно исправить код так:
-const harrysAccount = new BankAccount(100);
-harrysAccount.deposit.call(harrysAccount, 200); 
-harrysAccount.deposit.apply(harrysAccount, [1000]);
+const harrysAccount = new BankAccount(100)
+harrysAccount.deposit.call(harrysAccount, 200) 
+harrysAccount.deposit.apply(harrysAccount, [1000])
 
 console.log(harrysAccount)
