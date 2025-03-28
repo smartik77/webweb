68ece91aec399920aec67304f6ffac62bef2ed90
Anthony <145694891+smartik77@users.noreply.github.com>
2025-03-28 12:32:44 +0300

    Update js10.js

--- js10.js
+++ js10.js
@@ -1,65 +1,65 @@
 class BankAccount {
      constructor(balance = 0) {
-          this.balance = balance; // Начальный баланс
+          this.balance = balance  // Начальный баланс
      }
 
      // Пополнение счета (возвращает true/false при успехе/ошибке)
      deposit(amount) {
           if (amount > 0) {
-               this.balance += amount;
+               this.balance += amount
             
-               console.log('Баланс пополнен на ', amount);
-               console.log('Счет:', this.balance);
-               return true;
+               console.log('Баланс пополнен на ', amount)
+               console.log('Счет:', this.balance)
+               return true
           }
-          return false;
+          return false
      }
 
      // Снятие средств (проверяет доступный баланс)
      withdraw(amount) {
           if (amount > 0 && this.balance >= amount) {
-               this.balance -= amount;
-               console.log('С баланса снято ', amount);
-               console.log('Счет:', this.balance);
-               return true;
+               this.balance -= amount
+               console.log('С баланса снято ', amount)
+               console.log('Счет:', this.balance)
+               return true
           }
-          return false;
+          return false
      }
 
      // Текущий баланс
      getBalance() {
-          console.log('На счету:', this.balance);
-          return this.balance;
+          console.log('На счету:', this.balance)
+          return this.balance
      }
 }
 
 // Сберегательный счет с процентами
 class SavingsAccount extends BankAccount {
      constructor(balance = 0, interestRate = 0) {
-          super(balance);
-          this.interestRate = interestRate; // Процентная ставка (например, 0.05 = 5%)
+          super(balance)
+          this.interestRate = interestRate // Процентная ставка (например, 0.05 = 5%)
      }
 
      // Начислить проценты через метод deposit()
      addInterest() {
-          const interest = this.getBalance() * this.interestRate;
-          this.deposit(interest); // Используем метод родителя
+          const interest = this.getBalance() * this.interestRate
+          this.deposit(interest) // Используем метод родителя
      }
 }
 
 // Текущий счет с комиссией за снятие
 class CheckingAccount extends BankAccount {
      constructor(balance = 0, fee = 0) {
-          super(balance);
-          this.fee = fee; // Фиксированная комиссия за операцию
+          super(balance)
+          this.fee = fee // Фиксированная комиссия за операцию
      }
 
      // Переопределяем withdraw() с добавлением комиссии
      withdraw(amount) {
-          return super.withdraw(amount + this.fee); // Используем родительский метод
-          console.log('С баланса снято ', amount);
-          console.log('комиссия:  ',  this.fee);
-          console.log('Счет:', this.balance);
+          return super.withdraw(amount + this.fee) // Используем родительский метод
+          console.log('С баланса снято ', amount)
+          console.log('комиссия:  ',  this.fee)
+          console.log('Счет:', this.balance)
      }
 }
 
