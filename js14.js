b131f7395d9a24fe9b7752b6fd1feae6b1cd7164
Anthony <145694891+smartik77@users.noreply.github.com>
2025-03-28 13:19:11 +0300

    Update js14.js

--- js14.js
+++ js14.js
@@ -1,42 +1,38 @@
-// Базовый класс банковского счета
 class BankAccount {
-  constructor(balance = 0) {
-    this.balance = balance; // Текущий баланс
-  }
-
-  // Пополнение счета
-  deposit(amount) {
-    if (amount > 0) {
-      this.balance += amount;
-      console.log('Баланс пополнен на ', amount);
-      console.log('Счет:', this.balance);
-      return true;
+    constructor(balance = 0) {
+        this.balance = balance
     }
-    return false;
-  }
 
-  // Получить баланс
-  getBalance() {
-    return this.balance;
-  }
+    deposit(amount) {
+        if (amount > 0) {
+            this.balance += amount
+            return true
+        }
+        return false
+    }
 }
 
-
-
-
-
-// Утилита для отложенного вызова функции с аргументом delay
-function invokeLater(f, delay) {
-  setTimeout(() => f(delay), delay); 
-}
-
-//  Функция для пополнения любого счета
+// Функция для создания обработчика пополнения счета
 function depositInto(account) {
-  return (amount) => account.deposit(amount); // Возвращает функцию с привязанным account
+    return amount => account.deposit(amount)
 }
 
-// Пример использования
-const sallysAccount = new BankAccount(1000);
+// Функция для отложенного вызова
+function invokeLater(f, delay) {
+    setTimeout(() => f(delay), delay)
+}
+
+// Создаем аккаунты
+const harrysAccount = new BankAccount(100)
+const sallysAccount = new BankAccount(500)
+
+// Гарри получает 1000 через 1000 мс
+invokeLater(depositInto(harrysAccount), 1000)
+// Салли получает 2000 через 2000 мс
+invokeLater(depositInto(sallysAccount), 2000)
 
 
-invokeLater(depositInto(sallysAccount), 2000);
+setTimeout(() => {
+    console.log("Баланс Гарри:", harrysAccount.balance)
+    console.log("Баланс Саллу:", sallysAccount.balance) 
+}, 3000)
