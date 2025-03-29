cd883f4ff9105215396862f0d02dee180e5d7ca2
Anthony <145694891+smartik77@users.noreply.github.com>
2025-03-29 10:00:12 +0300

    Update 1-3.php

--- php/1-3.php
+++ php/1-3.php
@@ -1,11 +1,12 @@
 <?php
-$array = [1, 2, 3, 2, 4, 2, 5];
-$valueToRemove = 2;
-
-foreach ($array as $key => $value) {
-    if ($value === $valueToRemove) {
-        unset($array[$key]); // удаление элемента 
+    $array = [1, 2, 3, 2, 4, 2, 5];
+    $valueToRemove = 2;
+    
+    foreach ($array as $key => $value) {
+        if ($value === $valueToRemove) {
+            unset($array[$key]); // удаление элемента 
+        }
     }
-}
-
-print_r($array);
+    
+    print_r($array);
+?>
