b152a9897d479c0fd9ed75585e40ac3ad3df9f2d
Anthony <145694891+smartik77@users.noreply.github.com>
2025-03-29 09:13:06 +0300

    Create 1-2.php

---
+++ php/1-2.php
@@ -0,0 +1,12 @@
+<?php
+for ($i = 1; $i < 1000; $i++) {
+    $str = (string)$i;
+    
+    // Не испольльзуем числа в которых нет 2 цифр
+    if (strlen($str) < 2) { continue; }
+    
+    if ((int)$str[0] + (int)$str[1] === 5) {
+        echo $str . "\n";
+    }
+}
+?>
