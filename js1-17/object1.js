b68b6995bda3ab42a7d7f611136aed815ef3fadf
Anthony <145694891+smartik77@users.noreply.github.com>
2025-04-12 09:22:24 +0300

    Create object1.js

---
+++ js1-17/object1.js
@@ -0,0 +1,15 @@
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+
+let sum = 0
+if (salaries.length === 0) {
+    sum = 0
+}
+for (let key in salaries) {
+  sum += salaries[key]
+}
+
+console.log(sum)
